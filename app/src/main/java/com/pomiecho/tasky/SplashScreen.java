6ddeb94160a8e6b72644cb4d4e4c4985057c4873
przemyslaw <p.pomiechowski@outlook.com>
2020-10-22 00:51:08 +0200

    1. New Splash Screen (font Polly_Rounded) 2. Background color for main part of app (gradient used in .xml files)

---
+++ app/src/main/java/com/pomiecho/tasky/SplashScreen.java
@@ -0,0 +1,37 @@
+package com.pomiecho.tasky;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.View;
+import android.view.Window;
+
+public class SplashScreen extends AppCompatActivity {
+
+    private final int SPLASH_SCREEN_DURATION = 3000;
+
+    private Window window;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        window = getWindow();
+        window.getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+        );
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this,
+                        MainActivity.class);
+                SplashScreen.this.startActivity(intent);
+                SplashScreen.this.finish();
+            }
+        }, SPLASH_SCREEN_DURATION);
+    }
+}
